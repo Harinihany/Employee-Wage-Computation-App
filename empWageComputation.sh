f247316c7770fac3627b762d7a7d8034b3c78b5f
Harinihany <harinihany03@gmail.com>
2020-11-21 17:12:47 +0530

    Added: calculating salary logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,6 +7,12 @@ echo "Random number: "$random
 if(( $random == $isPresent ))
 then
 echo "Employee is Present!"
+empHrs=8
+ratePerHour=20
+salary=$(( $empHRS * $ratePerHour ))
 else
 echo "Employee is absent!"
+salary=0
 fi
+echo "salary: $salary"
+
