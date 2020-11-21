ff577b8d380824f9f01baa6e9b7db96b1b17509c
Harinihany <harinihany03@gmail.com>
2020-11-21 21:40:59 +0530

    modified: Employee Wage using for loop

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,7 +4,10 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 IS_PART_TIME=1
 IS_FULL_TIME=2
 EMP_RATE_PER_HR=20
-
+NUM_WORKING_DAYS=20
+totalSalary=0
+for(( day=1 ; day <= $NUM_WORKING_DAYS; day++ ))
+do
 random=$(( RANDOM%3 ))
 echo "Random number: "$random
 case $random in
@@ -14,9 +17,12 @@ $IS_FULL_TIME) empHrs=8
 ;;
 *) empHrs=0
 esac
-echo "Employee hours: " $empHrs
-
 salary=$(( $empHRS * $EMP_RATE_PER_HR ))
+echo "Day: $day salary: $salary"
+totalSalary=$(( $totalSalary + $salary ))
+done
 
-echo "salary: $salary"
+echo "Total Salary: $totalSalary"
+
+
 
